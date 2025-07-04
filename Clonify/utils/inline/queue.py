615a31f01a157ccf186f344510a5a5a3ab14b23d
ISTKHAROP <istkharalam2006@gmail.com>
2025-07-04 16:25:38 +0530

    Update queue.py

--- Clonify/utils/inline/queue.py
+++ Clonify/utils/inline/queue.py
@@ -113,7 +113,7 @@ def queuemarkup(_, vidid, chat_id):
         [
             InlineKeyboardButton(
                 text="๏ ᴍᴏʀᴇ ๏",
-                url="https://t.me/PURVI_SUPPORT",
+                url="https://t.me/ARISHFA_UPDATE",
             ),
         ],
     ]
